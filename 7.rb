21e7ec6999ca639e8b0f843a84df09b3cad77a92
sala4187 <sarah.lacoste@colorado.edu>
2014-02-03 19:52:24 -0700

    Update 7.rb

--- 7.rb
+++ 7.rb
@@ -17,13 +17,13 @@ class Dessert
   end
 end
 
+puts "7(a)"
 cookie=Dessert.new("cookie",150)
 puts "cookie delicious?"
 puts cookie.delicious?
 puts "cookie healthy?"
 puts cookie.healthy?
 
-puts " "
 cake=Dessert.new("cake",250)
 puts "cake delicious?"
 puts cake.delicious?
@@ -34,6 +34,7 @@ puts cake.healthy?
 
 class JellyBean < Dessert
   attr_accessor :flavor
+
   def initialize(name, calories, flavor)
     @name=name 
     @calories=calories
@@ -50,16 +51,15 @@ class JellyBean < Dessert
 end
 
 puts " "
+puts "7(b)"
 black_licorice=JellyBean.new("a",150,"black licorice")
 puts "black licorice delicious?"
 puts black_licorice.delicious?
 puts "black licorice healthy?"
 puts black_licorice.healthy?
 
-puts " "
 watermellon=JellyBean.new("a",250,"watermellon")
 puts "watermellon delicious?"
 puts watermellon.delicious?
 puts "watermellon healthy?"
 puts watermellon.healthy?
-
