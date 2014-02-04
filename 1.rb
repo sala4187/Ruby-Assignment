4b772e4c50f7f2c3f2d729931b26da983c770670
sala4187 <sarah.lacoste@colorado.edu>
2014-02-03 19:48:37 -0700

    Update 1.rb

--- 1.rb
+++ 1.rb
@@ -1,13 +1,13 @@
 #(1)
 
-class HelloWorldClass
-    def initialize(name)
-       @name = name.capitalize
+class HelloWorldClass # create class "HelloWorldClass"
+    def initialize(name) # define method initialize that takes in name
+       @name = name.capitalize # create instance variable name and capatalize it
     end
-    def sayHi
-        puts "Hello #{@name}!"
+    def sayHi # define method sayHi
+        puts "Hello #{@name}!" #prints Hello and gets name
     end
 end
 
-hello = HelloWorldClass.new("{sarah}")
-hello.sayHi
+hello = HelloWorldClass.new("sarah") # create new hello world class sarah
+hello.sayHi 
