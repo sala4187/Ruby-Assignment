3a0a8c1c717713e15c1043aeec6e35f480709f87
sala4187 <sarah.lacoste@colorado.edu>
2014-02-03 19:53:21 -0700

    Update 8.rb

--- 8.rb
+++ 8.rb
@@ -1,20 +1,20 @@
 # (8) 
 
 class Class
+	def attr_accessor_with_history(attr_name)
+        	attr_name = attr_name.to_s       # make sure it's a string
+    		attr_reader attr_name            # create the attribute's getter
+        	attr_reader attr_name + "_history" # create bar_history getter
 
-    def attr_accessor_with_history(attr_name)
+        	class_eval %Q{ # 
+      			def #{attr_name}=(new_value)
+        		@#{attr_name}_history ||= [nil] 
+        		@#{attr_name}_history << @#{attr_name} = new_value
+      			end
+    			}
+    		end
 
-        attr_name = attr_name.to_s       # make sure it's a string
-
-        attr_reader attr_name            # create the attribute's getter
-
-        attr_reader attr_name+"_history" # create bar_history getter
-
-        class_eval "your code here, use %Q for multiline strings"
-
-    end
-
-end
+	end
 
  
 
@@ -32,5 +32,4 @@ f.bar = 1
 
 f.bar = 2
 
-f.bar_history # => if your code works, should be [nil, 1, 2]
-
+p f.bar_history # => if your code works, should be [nil, 1, 2]
