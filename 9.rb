f15ae4e8e59f033856e26641b2ecae7d698f3ea4
sala4187 <sarah.lacoste@colorado.edu>
2014-02-03 19:53:52 -0700

    Update 9.rb

--- 9.rb
+++ 9.rb
@@ -1,25 +1,29 @@
 # (9) — Currency conversion.  
 
 class Numeric
+@@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019, 'dollar' => 1}
+	def method_missing(diff_currency)
+   		singular_currency = diff_currency.to_s.gsub( /s$/, '') # remove last s to define singular currency
+   		if @@currencies.has_key?(singular_currency) # returns true if given key is present
+     			self * @@currencies[singular_currency]
+  		else
+     			super # calls method of parent class if it exists
+  		end
 
- @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+ 	end
 
- def method_missing(method_id)
-
-   singular_currency = method_id.to_s.gsub( /s$/, '')
-
-   if @@currencies.has_key?(singular_currency)
-
-     self * @@currencies[singular_currency]
-
-   else
-
-     super
-
-   end
-
- end
+	def in(currency)
+		singular_currency = currency.to_s.gsub( /s$/, '') # remove last s
+    		if @@currencies.has_key?(singular_currency)
+      			self / @@currencies[singular_currency]
+    		else
+      			super
+    		end
+	end	
 
 end
 
+p 5.dollars.in(:euros)
+p 10.euros.in(:rupees)
+p 1.euro.in(:rupees)
  
